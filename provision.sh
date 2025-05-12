37de3528c845f9d3d5aff4063e13f44365bf2b14
Mike Thomas <miket0788psn@gmail.com>
2025-05-12 14:29:46 -0500

    Moved components to docker-compose from provision.sh to simplify script

--- provision.sh
+++ provision.sh
@@ -16,23 +16,6 @@ sudo apt-get update
 # Install Docker:
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Add the current user to the docker group:
-sudo docker run -it --rm -d -p 8080:80 --name web nginx
-
-# Check if the container is running:
-docker run -d --name alloy \
-  -v /vagrant/config.alloy:/etc/alloy/config.alloy \
-  -p 12345:12345 \
-  grafana/alloy:latest \
-    run --server.http.listen-addr=0.0.0.0:12345 --storage.path=/var/lib/alloy/data \
-    /etc/alloy/config.alloy
-
-# Check if the container is running:
-docker run -d --name prometheus \
-    -p 9090:9090 \
-    -v /vagrant/prometheus.yml:/etc/prometheus/prometheus.yml \
-    prom/prometheus
-
 # Check if the container is running:
 cd /vagrant
 docker compose up -d
